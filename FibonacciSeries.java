cf92646354ec2a8b432521d7d4b37f30f7a702d0
Tiju George <tiju.m.george@gmail.com>
2017-04-18 21:04:52 -0400

    Create FibonacciSeries.java

---
+++ FibonacciSeries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class FibonacciSeries { 
+
+   public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+       System.out.print("Enter the value of n: ");
+       int n = s.nextInt();
+       for (int i = 0; i <= n; i++) {
+           System.out.print(fibonacci(i) + " ");
+       }
+   }
+
+   public static int fibonacci(int n) {
+       if (n == 0) {
+           return 0;
+       } else if (n == 1) {
+           return 1;
+       } else {
+           return fibonacci(n - 1) + fibonacci(n - 2);
+       }
+   }
+}
