7bd78b8bbd77b1bcf9844cfde53035981d8eeb28
Tiju George <tiju.m.george@gmail.com>
2017-04-18 21:16:25 -0400

    second commit

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class FibonacciSeries { 
+public class Main { //Main is FibonacciSeries
 
    public static void main(String[] args) {
        Scanner s = new Scanner(System.in);
@@ -12,11 +12,10 @@ public class FibonacciSeries {
    }
 
    public static int fibonacci(int n) {
-       if (n == 0) {
-           return 0;
-       } else if (n == 1) {
-           return 1;
-       } else {
+       if ((n == 0) || (n == 1)){
+           return n;
+       } 
+       else {
            return fibonacci(n - 1) + fibonacci(n - 2);
        }
    }
