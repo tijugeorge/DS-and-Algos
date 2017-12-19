a5a40958ab05e35418c793fddf452ffbea5d7a44
Tiju George <tiju.m.george@gmail.com>
2017-12-19 15:23:57 -0500

    Create elseif.java

---
+++ JavaPractice-HeadfirstJava/elseif.java
@@ -0,0 +1,14 @@
+class apples{
+	public static void main(String[] args){
+		int age = 55;
+
+		if (age > 60){
+			System.out.println("You are a senior citizen");
+		}
+		else if (age >= 50){
+			System.out.println("You are in 50s");
+		}else {
+			System.out.println("You are a young guy");
+		}
+	}
+}
