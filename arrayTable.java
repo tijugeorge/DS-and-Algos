2248d0129d5aa662ebd62241c1afd87e2bc383aa
Tiju George <tiju.m.george@gmail.com>
2017-12-22 15:18:06 -0500

    Create arrayTable.java

---
+++ arrayTable.java
@@ -0,0 +1,10 @@
+class apples{
+	public static void main(String[] args){
+		System.out.println("Index\tValue");
+		int bucky[] = {32,15,23,4,56,34};
+
+		for (int counter=0; counter < bucky.length; counter++){
+			System.out.println(counter + "\t" + bucky[counter]);
+		}
+	}
+}
