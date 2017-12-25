5f4e70fae3bd31445cf1383b68c3341215a935e3
Tiju George <tiju.m.george@gmail.com>
2017-12-24 21:37:13 -0500

    Create guiapples.java

---
+++ JavaPractice-HeadfirstJava/guiapples.java
@@ -0,0 +1,26 @@
+/*1
+import javax.swing.JFrame;
+
+
+class apples{
+	public static void main(String[] args){
+
+		guituna bucky = new guituna();
+		bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		bucky.setSize(275, 180);
+		bucky.setVisible(true);
+	}
+}
+
+*/
+import javax.swing.JFrame;
+
+class guiapples{
+	public static void main(String[] args){
+		guituna bucky = new guituna();
+		bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		bucky.setSize(350, 100);
+		bucky.setVisible(true);
+
+	}
+}
