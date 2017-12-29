b05b2ff297b48cd52206ae1f48cfed838f6b8f94
Tiju George <tiju.m.george@gmail.com>
2017-12-28 20:56:30 -0500

    Create mine_sweeper.py

---
+++ udemy11imp/mine_sweeper.py
@@ -0,0 +1,40 @@
+
+def mine_sweeper(bombs, num_rows, num_cols):
+	field = [[0 for i in range(num_cols)] for j in range(num_rows)]
+	for bomb in bombs:
+		row_i = bomb[0]
+		col_i = bomb[1]
+		field[row_i][col_i] = -1
+		for i in range(row_i-1, row_i+2):
+			for j in range(col_i-1, col_i+2):
+				if (0 <= i < num_rows and 0 <= j < num_cols and field[i][j] != -1):
+					field[i][j] += 1
+	return field
+
+def toString(given_array):
+	list_rows = []
+	for row in given_array:
+		list_rows.append(str(row))
+	return '[' + '\n'.join(list_rows) + ']'
+
+
+
+# NOTE: The following input values will be used for testing your solution.
+print(toString(mine_sweeper([[0, 2], [2, 0]], 3, 3)))
+# [[0, 1, -1],
+#  [1, 2, 1],
+#  [-1, 1, 0]]
+print("---------------------------")
+
+print(toString(mine_sweeper([[0, 0], [0, 1], [1, 2]], 3, 4)))
+# [[-1, -1, 2, 1],
+#  [2, 3, -1, 1],
+#  [0, 1, 1, 1]]
+print("---------------------------")
+
+print(toString(mine_sweeper([[1, 1], [1, 2], [2, 2], [4, 3]], 5, 5)))
+# [[1, 2, 2, 1, 0],
+#  [1, -1, -1, 2, 0],
+#  [1, 3, -1, 2, 0],
+#  [0, 1, 2, 2, 1],
+#  [0, 0, 1, -1, 1]]
