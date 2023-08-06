650bf7a2a87b51d385a346c288a30f9e237144f8
Tiju Mathew <tiju.m.george@gmail.com>
2023-08-06 00:56:48 -0400

    Create rangeSumBST.py

---
+++ Tree/rangeSumBST.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        if not root:
+            return 0
+
+        ans = 0
+        if low <= root.val <= high:
+            ans += root.val
+        if low < root.val:
+            ans += self.rangeSumBST(root.left, low, high)
+        if root.val < high:
+            ans += self.rangeSumBST(root.right, low, high)
+
+        return ans
