6003b353260d1f1234c30a68569e18077a4cb628
Tiju Mathew <tiju.m.george@gmail.com>
2023-08-08 17:21:01 -0400

    Create minReorder.py

---
+++ Tree/minReorder.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        roads = set()
+        graph = defaultdict(list)
+        for x, y in connections:
+            graph[x].append(y)
+            graph[y].append(x)
+            roads.add((x,y))
+
+        def dfs(node):
+            ans = 0
+            for neighbor in graph[node]:
+                if neighbor not in seen:
+                    if (node, neighbor) in roads:
+                        ans += 1
+                    seen.add(neighbor)
+                    ans += dfs(neighbor)
+            
+            return ans
+
+
+        seen = {0}
+        return dfs(0)
