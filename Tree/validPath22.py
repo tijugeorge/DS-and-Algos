f7afd1954273db6cf8fd9b8414b9baf198f1562b
Tiju Mathew <tiju.m.george@gmail.com>
2023-08-11 07:37:19 -0400

    Create validPath22.py

---
+++ Tree/validPath22.py
@@ -0,0 +1,21 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = collections.defaultdict(list)
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        print(graph)    
+        seen = [False]*n
+        
+        def dfs(curr_node):
+            if curr_node == destination:
+                return True
+            if not seen[curr_node]:
+                seen[curr_node] = True
+                for next_node in graph[curr_node]:
+                    if dfs(next_node):
+                        return True
+            return False
+        return dfs(source)
+
+  #recursive
