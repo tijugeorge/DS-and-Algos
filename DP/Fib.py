ce4f0f29a46ef7cf31bfddcd802fe23165c63c4a
Tiju Mathew <tiju.m.george@gmail.com>
2023-08-18 17:14:02 -0400

    Create Fib.py

---
+++ DP/Fib.py
@@ -0,0 +1,34 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+####################################
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    if n in memo:
+        return memo[n]
+    
+    memo[n] = fibonacci(n - 1) + fibonacci(n - 2)
+    return memo[n]
+
+memo = {}
+
+#######################################
+
+def fibonacci(n):
+    arr = [0] * (n + 1)
+    # base case - the second Fibonacci number is 1
+    arr[1] = 1
+    for i in range(2, n + 1):
+        arr[i] = arr[i - 1] + arr[i - 2]
+    
+    return arr[n]
