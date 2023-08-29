a0107899011b78390b095ea528e91bf252891311
Tiju Mathew <tiju.m.george@gmail.com>
2023-08-28 20:05:28 -0400

    Create combinationSum.py

---
+++ BT/combinationSum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        def backtrack(path, start, curr):
+            if curr == target:
+                ans.append(path[:])
+                return
+            
+            for i in range(start, len(candidates)):
+                num = candidates[i]
+                if curr + num <= target:
+                    path.append(num)
+                    backtrack(path, i, curr+num)
+                    path.pop()
+
+            ans = []
+            backtrack([], ,0, 0)
+            return ans
+                
